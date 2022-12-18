349ebe91fc6bb2831ac60a8cb60419d516cd8c82
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2022-12-18 13:47:35 +0100

    Update mapdata-hos_velen.js

--- files/scripts/mapdata-hos_velen.js
+++ files/scripts/mapdata-hos_velen.js
@@ -2262,8 +2262,8 @@ window.mapdata_hos_velen = {
 		popup:$.t("scavenger.dia") + $.t("scavenger.name.urs") + $.t("scavenger.item.armor") + $.t("scavenger.level.3") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.urs") + $.t("scavenger.item.gaunt") + $.t("scavenger.level.3") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.urs") + $.t("scavenger.item.trous") + $.t("scavenger.level.3") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.urs") + $.t("scavenger.item.boot") + $.t("scavenger.level.3") + $.t("<br />") + $.t("misc.entrance1",{x1: $.t("<a href='#6/142.984/118.125\'>"), end: $.t("</a>")})
 	},{
 		coords: [[141.453,119.125]],
-		label: $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.level.1"),
-		popup:$.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.armor") + $.t("scavenger.level.1") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.gaunt") + $.t("scavenger.level.1") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.trous") + $.t("scavenger.level.1") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.boot") + $.t("scavenger.level.1")
+		label: $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.level.6"),
+		popup:$.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.armor") + $.t("scavenger.level.6") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.gaunt") + $.t("scavenger.level.6") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.trous") + $.t("scavenger.level.6") + $.t("<br />") + $.t("scavenger.dia") + $.t("scavenger.name.fgrwol") + $.t("scavenger.item.boot") + $.t("scavenger.level.6")
 	}],
 
 	//------------------------------------------------Shopkeeper------------------------------------------------
